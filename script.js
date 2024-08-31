03b70c2259c5a9a9d52c6877e3cbf327ae817669
sakibtamim <you@example.com>
2024-09-01 03:55:49 +0600

    2nd commit

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
+/* local quotes */
+//Show new Quotes
+function newQuotes() {
+    //Pick a random quote 
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    console.log(quote);
+}
+
+
+newQuotes();
+
+
+
+
+
+/* Api quotes */
+/*
 let apiQuotes = []
 
 // Get Quotes From API
 async function getQuotes() {
     //const apiUrl = 'https://api.api-ninjas.com/v1/quotes';
-    // const apiUrl = 'https://quotes-react.netlify.app';
-    const apiUrl = 'https://type.fit/api/quotes';
+    const apiUrl = 'https://dummyjson.com/quotes/random';
+    //const apiUrl = 'https://type.fit/api/quotes';
 
     try {
-        const response = await fetch(apiUrl, /*{
-
-            headers: {
-                'X-Api-Key': 'oYbmJipEF8P7Q13AcrY+oQ==eHQqXJ6d5rshEXKL'
-            }
-        }*/);
+        const response = await fetch(apiUrl
+            //{
+            // headers: {
+            //'X-Api-Key': 'oYbmJipEF8P7Q13AcrY+oQ==eHQqXJ6d5rshEXKL'
+            // }
+            //}
+        );
         apiQuotes = await response.json();
-        console.log(apiQuotes[0]);
+        console.log(apiQuotes);
     } catch (error) {
-        console.log(error)
+        console.log(error);
         //Catch error here
     }
 }
 
 //On LOAD
 getQuotes();
+*/
